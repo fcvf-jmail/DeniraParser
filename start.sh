2e598b2fa2920c3377e361e90b8b6e690afccc16
Jmail El Kazzaz <gamelelkazzaz@gmail.com>
2024-11-15 01:33:55 +0300

    updated sh script to restart automaticly

--- start.sh
+++ start.sh
@@ -5,6 +5,16 @@ touch chatId.txt
 touch .env
 touch lastNearestDate.txt
 
+# Проверяем, запущен ли контейнер denira-parser
+CONTAINER_ID=$(docker ps -q -f name=denira-parser)
+
+# Если контейнер запущен, останавливаем и удаляем его
+if [ -n "$CONTAINER_ID" ]; then
+  echo "Контейнер denira-parser уже запущен с ID $CONTAINER_ID. Останавливаем его..."
+  docker stop "$CONTAINER_ID"
+  docker rm "$CONTAINER_ID"
+fi
+
 # Запускаем Docker Compose с командой для сервиса denira-parser
 docker-compose run --rm denira-parser
 
